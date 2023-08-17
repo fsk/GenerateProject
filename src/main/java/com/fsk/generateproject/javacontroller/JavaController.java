85c1a2b04597d26e31bf910ae055607d72bf2dc0
0xfsk <furkankulaksiz68@gmail.com>
2023-08-17 19:44:15 +0300

    initial commit 2

--- src/main/java/com/fsk/generateproject/javacontroller/JavaController.java
+++ src/main/java/com/fsk/generateproject/javacontroller/JavaController.java
@@ -1,2 +1,18 @@
-package com.fsk.generateproject.javacontroller;public class JavaController {
+package com.fsk.generateproject.javacontroller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/java-controller")
+@RequiredArgsConstructor
+public class JavaController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Project";
+    }
+
 }
